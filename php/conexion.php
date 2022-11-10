c42e607ee8581f2bf712233b2345910f2ea618b0
jdmarinv <jdmarinv@gmail.com>
2022-11-10 17:11:57 -0600

    Primer commit
    Publicación de contenido para prueba técnica.

---
+++ php/conexion.php
@@ -0,0 +1,8 @@
+<?php
+$host="localhost";
+$user="root";
+$password="";
+$db="opsa";
+$con = new mysqli($host,$user,$password,$db);
+
+?>
---
+++ php/navbar.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-dark bg-dark" role="navigation">
+<div class="container">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <a class="navbar-brand" href="./"><b>El Heraldo</b></a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <?php if(!isset($_SESSION["user_id"])):?>
+      <li><a href="./registro.php">REGISTRO</a></li>
+      <li><a href="./login.php">LOGIN</a></li>
+    <?php else:?>
+      <li><a href="./php/logout.php">SALIR</a></li>
+    <?php endif;?>
+    </ul>
+
+  </div><!-- /.navbar-collapse -->
+</div>
+</nav>
